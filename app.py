4724486ec6f7511d01c05d3787ec88cee2dcea03
Germinary <maxgromov10@yandex.ru>
2024-01-18 08:12:09 +0500

    Add 1 boss template

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sys
 import os
 from config import config
 from time import sleep
+from random import randint
 
 pygame.init()
 size = width, height = 1000, 600
@@ -19,15 +20,16 @@ sounds = {
 	'dash': pygame.mixer.Sound('data/sounds/hero_dash.wav'),
 	'land': pygame.mixer.Sound('data/sounds/hero_land.wav'),
 	'kick': pygame.mixer.Sound('data/sounds/hero_kick.wav'),
-	'ui': pygame.mixer.Sound('data/sounds/other_ui.wav')
+	'ui': pygame.mixer.Sound('data/sounds/other_ui.wav'),
+	'magic': pygame.mixer.Sound('data/sounds/hero_magic.wav')
 }
 music = {
 	'menu': 'data/music/menu.mp3',
-	'level0': 'data/music/level0.mp3'
+	'level0': 'data/music/level0.mp3',
+	'level1': 'data/music/level1.mp3',
+	'level2': 'data/music/level2.mp3', 
+	'level3': 'data/music/level3.mp3',
 }
-levels = [
-	['level0']
-]
 
 # Константы
 FPS = 60
@@ -98,7 +100,7 @@ class Magic(pygame.sprite.Sprite):
 		self.frame = 0
 		self.cur_frame = 0
 		self.direction = data[-1][0]
-		self.v = 700
+		self.v = 1200
 	def update(self):
 		self.frame += 1
 		if self.frame % 4 == 0:
@@ -120,7 +122,7 @@ class MainCharacter(pygame.sprite.Sprite):
 
 	def __init__(self, *group):
 		super().__init__(*group)
-		self.image = MainCharacter.image
+		self.image = pygame.transform.flip(MainCharacter.image, True, False)
 		self.rect = self.image.get_rect()
 		self.rect.x = 100
 		self.rect.y = height - self.image.get_height() - 100
@@ -139,10 +141,13 @@ class MainCharacter(pygame.sprite.Sprite):
 		self.dash = 300
 
 		self.is_moving = False
-		self.direction = -1
+		self.direction = 1
 		self.grounded = False
+		self.magic_cooldown = 1000
 
 	def update(self):
+		if self.magic_cooldown < 1000:
+			self.magic_cooldown += 300 / FPS
 		if not self.grounded and self.on_ground() and self.jump_fase >= self.gravity:
 			sounds['land'].play()
 			self.grounded = True
@@ -208,16 +213,147 @@ class MainCharacter(pygame.sprite.Sprite):
 				self.cur_frame -= self.cur_frame // lim * lim
 			self.rect.x += self.direction * self.v / FPS
 
+
+class Nail(pygame.sprite.Sprite):
+	image = load_image('other/nail/0.bmp', colorkey=-1)
+	def __init__(self, coords, direction):
+		super().__init__(all_sprites)
+		self.direction = direction
+		if self.direction == -1:
+			self.image = Nail.image
+		else:
+			mirrored = pygame.transform.flip(Nail.image, True, False)
+			self.image = mirrored
+		self.rect = self.image.get_rect()
+		self.rect.x = coords[0]
+		self.rect.y = coords[1]
+		self.v = 1000
+
+	def update(self):
+		self.rect.x += self.direction * self.v / FPS
+
+		if self.rect.x < -100 or self.rect.x > width:
+			self.kill()
+		if self.rect.y > height + 100 or self.rect.y < -100:
+			self.kill()
+
+
+class PureVessel(pygame.sprite.Sprite):
+	image = load_image('bosses/pure_vessel/0.bmp', colorkey=-1)
+	attacks_ = [
+		load_image('bosses/pure_vessel/1.bmp', colorkey=-1),
+		load_image('bosses/pure_vessel/2.bmp', colorkey=-1),
+		load_image('bosses/pure_vessel/3.bmp', colorkey=-1)
+	]
+	def __init__(self, *group):
+		super().__init__(*group)
+		self.image = PureVessel.image
+		self.rect = self.image.get_rect()
+		self.rect.x = 700
+		self.rect.y = height - self.image.get_height() - 100
+		self.dash = 1000
+		self.direction = -1
+		self.v = 1500
+		self.attacks = False
+		self.last_attack = 0
+		self.delay = 0
+		self.nails_buffer = []
+	
+	def set_target(self, target):
+		self.target = target
+
+	def attack(self, type):
+		self.attacks = True
+		if type == 0:
+			if self.direction == -1:
+				self.rect.x = 700
+				self.image = self.attacks_[0]
+			else:
+				self.rect.x = 50
+				mirrored = pygame.transform.flip(self.attacks_[0], True, False)
+				self.image = mirrored
+			self.delay = 500
+			self.dash = 0
+		if type == 1:
+			self.image = self.attacks_[1]
+			self.rect.x = self.target.rect.x + self.target.image.get_width() // 2 - self.image.get_width() // 2
+			self.rect.y = 25
+			self.delay = 500
+		if type == 2:
+			nails_coords = []
+			if self.direction == -1:
+				self.image = self.attacks_[2]
+				self.rect.x = 700
+				for i in range(-1, 9):
+					x = self.rect.x - Nail.image.get_width() - 50
+					y = self.rect.y + i * 100
+					nails_coords.append((x, y))
+			elif self.direction == 1:
+				mirrored = pygame.transform.flip(self.attacks_[2], True, False)
+				self.image = mirrored
+				self.rect.x = 50
+				for i in range(-1, 9):
+					x = self.rect.x + self.image.get_width() + 50
+					y = self.rect.y + i * 100
+					nails_coords.append((x, y))
+			self.delay = 1000
+			self.nails_buffer = nails_coords
+
+
+	def on_ground(self):
+		return not (self.rect.y + self.image.get_height() < 500)
+
+	def update(self):
+		if self.delay > 0:
+			self.delay -= 1000 / FPS
+			return
+
+		if self.dash < 1000:
+			self.rect = self.rect.move(self.direction * self.v / FPS, 0)
+			self.dash += 2200 / FPS
+		if self.dash >= 1000 and self.on_ground():
+			self.attacks = False
+			if self.direction == -1:
+				self.image = PureVessel.image
+			else:
+				mirrored = pygame.transform.flip(PureVessel.image, True, False)
+				self.image = mirrored
+		if not self.on_ground():
+			self.rect = self.rect.move(0, 1000 / FPS)
+		if self.last_attack < 1000:
+			self.last_attack += 750 / FPS
+		if self.last_attack >= 1000:
+			self.direction = -1 if randint(0, 1) == 0 else 1
+			self.attack(randint(0, 2))
+			self.last_attack = 0
+		if len(self.nails_buffer) > 0 and self.delay <= 0:
+			for i in self.nails_buffer:
+				Nail(i, self.direction)
+			self.nails_buffer.clear()
+
+
+levels = [
+	['level0', PureVessel],
+	['level1'],
+	['level2'],
+	['level3', PureVessel],
+]
+
 def level(level):
+	pygame.mixer.music.fadeout(500)
+	pygame.mixer.music.load(music[levels[level][0]])
+	pygame.mixer.music.play(-1)
+
 	all_sprites.empty()
-	background_image = load_image('levels/0/background.bmp')
+	background_image = load_image(f'levels/{level}/background.bmp')
 	background = UniqueSprite(all_sprites, background_image)
 
+	enemy = levels[level][1](all_sprites)
 	player = MainCharacter(all_sprites)
+	enemy.set_target(player)
 
 	running = True
-
-	music_started = False
+	enemy.direction = -1
 
 	while running:
 		all_sprites.draw(screen)
@@ -236,11 +372,14 @@ def level(level):
 				if event.key == config['dash'] and player.dash >= 800:
 					sounds['dash'].play()
 					player.dash = 0
-				if event.key == config['magic']:
+				if event.key == config['magic'] and player.magic_cooldown >= 1000:
 					data = [player.direction, [player.rect.x, player.rect.y]]
+					sounds['magic'].play()
+					player.magic_cooldown = 0
 					Magic(all_sprites, data)
 			else:
-				print(event)
+				pass
+				#print(event)
 		keys = pygame.key.get_pressed()
 
 		if keys[config['left']]:
@@ -256,11 +395,6 @@ def level(level):
 		clock.tick(FPS)
 		pygame.display.flip()
 
-		if not music_started:
-			pygame.mixer.music.fadeout(500)
-			pygame.mixer.music.load(music[levels[level][0]])
-			pygame.mixer.music.play(-1)
-			music_started = True
 	terminate()
 
 def menu():
